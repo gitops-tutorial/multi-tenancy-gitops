e1a0a9a2a66841af9e999a64b806af17ab45ca3f
Michael Spriggs <mspriggs@ca.ibm.com>
2022-04-05 14:13:50 -0400

    GitOps customizations for organization and cluster
    Signed-off-by: Michael Spriggs <mspriggs@ca.ibm.com>

--- scripts/set-git-source.sh
+++ scripts/set-git-source.sh
@@ -32,7 +32,7 @@ echo "Setting kustomization patches to ${GIT_BASEURL}/${GIT_ORG}/${GIT_GITOPS_AP
 find ${SCRIPTDIR}/../0-bootstrap -name '*.yaml' -print0 |
   while IFS= read -r -d '' File; do
     if grep -q "kind: Application\|kind: AppProject" "$File"; then
-      #echo "$File"
+      echo "$File"
       sed -i'.bak' -e "s#\${GIT_BASEURL}/\${GIT_ORG}/\${GIT_GITOPS}#${GIT_BASEURL}/${GIT_ORG}/${GIT_GITOPS}#" $File
       sed -i'.bak' -e "s#\${GIT_GITOPS_BRANCH}#${GIT_GITOPS_BRANCH}#" $File
       sed -i'.bak' -e "s#\${GIT_BASEURL}/\${GIT_ORG}/\${GIT_GITOPS_INFRA}#${GIT_BASEURL}/${GIT_ORG}/${GIT_GITOPS_INFRA}#" $File
